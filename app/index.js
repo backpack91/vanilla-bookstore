4a9470f7f2877388d850834077c2d2d919016142
backpack91 <bingrae1122@gmail.com>
2018-12-25 04:54:12 +0900

    commit for submit, complete vanillaBookStore

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-// Load application styles
+countForBookInfoReq// Load application styles
 import 'styles/index.less';
 
 // ================================
@@ -11,25 +11,24 @@ import bookChartTemplate from 'bookChart.ejs';
 import headerTemplate from 'header.ejs';
 import bodyTemplate from 'body.ejs';
 
-// Import Gorilla Module
 import Gorilla from '../Gorilla';
 
-var countForURLReq = 0;//url함수 비동기  count
-var urlList = [];//줄여진 url
-var bookInfos = [];//축적된 모든 -자료
-var infosToAdd = [];//추가적으로 받은 책 자료
-var countBookInfoReq = 0;//책자료 startIndex
-var whichType = 'list';
+var countForURLReq = 0;
+var shortenedUrlList = [];
+var bookInfoStorage = [];
+var infosToAdd = [];
+var countForBookInfoReq = 0;
+var chartType = 'list';
 var requestedloadMore = false;
 var gotResponse = true;
-var currentScrollY = 0;
+var isFirstRender = true;
 
 var header = new Gorilla.Component(headerTemplate, {
 });
 
 var bookChart = new Gorilla.Component(bookChartTemplate, {
   book: [],
-  whichType: whichType
+  chartType: chartType
 });
 
 var body = new Gorilla.Component(bodyTemplate, {
@@ -38,50 +37,73 @@ var body = new Gorilla.Component(bodyTemplate, {
   bookChart
 });
 
+body.topButt = function (event) {
+  scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
 header.clickByEnter = function (event) {
   var keyWord = document.querySelector('#input').value;
   if (event.keyCode === 13 && keyWord.length >= 1 && keyWord.length <= 20 && gotResponse) {
     gotResponse = false;
-    currentScrollY = 0;
-    bookInfos.length = 0;
-    countBookInfoReq = 0;
-    console.log('*****enter!*****');
+    bookInfoStorage.length = 0;
+    countForBookInfoReq = 0;
     makeRequest(keyWord);
+    document.querySelector('#header').classList.remove('header');
+    document.querySelector('#header').classList.add('headerAfterSearch');
+    document.querySelector('#title').classList.remove('title');
+    document.querySelector('#title').classList.add('titleAfterSearch');
   }
 }
 
 header.clickButt = function (event) {
   var keyWord = document.querySelector('#input').value;
-  if (event.keyCode === 13 && keyWord.length >= 1 && keyWord.length <= 20 && gotResponse) {
     gotResponse = false;
-    currentScrollY = 0;
-    bookInfos.length = 0;
-    countBookInfoReq = 0;
-    console.log('workingRequest2')
+    bookInfoStorage.length = 0;
+    countForBookInfoReq = 0;
     makeRequest(keyWord);
-  }
+    document.querySelector('#header').classList.remove('header');
+    document.querySelector('#header').classList.add('headerAfterSearch');
+    document.querySelector('#title').classList.remove('title');
+    document.querySelector('#title').classList.add('titleAfterSearch');
 }
 
 header.TransformToCard = function (event) {
-  bookChart.whichType = 'card';
+  bookChart.chartType = 'card';
 }
 
 header.TransformToList = function (event) {
-  bookChart.whichType = 'list';
+  bookChart.chartType = 'list';
 }
 
 window.addEventListener('scroll', function (event) {
-  var bookChart = document.querySelector('.bookChart');
+  var bookChart = document.querySelector('#bookChart');
+  var topButt = document.querySelector('.topButt');
+
   if ( !requestedloadMore && (bookChart.offsetHeight + bookChart.offsetTop + 100) === window.scrollY + window.innerHeight) {
     requestedloadMore = true;
     var keyWord = document.querySelector('#input').value;
     makeRequest(keyWord);
+  }
+  if (window.scrollY >500 && topButt.classList.contains('hide')) {
+    topButt.classList.remove('hide');
+  } else if (window.scrollY <= 500 && !topButt.classList.contains('hide')){
+    topButt.classList.add('hide');
   }
 });
 
 bookChart.on('AFTER_RENDER', () => {
   requestedloadMore = false;
   gotResponse = true;
+  if (isFirstRender) {
+    document.querySelector('#bookChart').classList.remove('bookChartAfterSearch');
+    document.querySelector('#bookChart').classList.add('bookChart');
+    isFirstRender = false;
+  } else {
+    document.querySelector('#bookChart').style.margin = '20px 0 100px 0';
+  }
 });
 
 Gorilla.renderToDOM ( body, document.querySelector('#root'));
@@ -91,33 +113,26 @@ function listUp (infoList) {
 }
 
 function infoModifier () {
-  console.log('*****infoModifier*****');
-  console.log('bookInfos: ', bookInfos);
-  for (var i = 0; i < urlList.length; i++) {
-    var oldDescription = bookInfos[i + (20 * (countBookInfoReq-1))].description;
-    var oldTitle = bookInfos[i + (20 * (countBookInfoReq-1))].title;
-    var oldImage = bookInfos[i + (20 * (countBookInfoReq-1))].image;
+  for (var i = 0; i < shortenedUrlList.length; i++) {
+    var oldDescription = bookInfoStorage[i + (20 * (countForBookInfoReq-1))].description;
+    var oldTitle = bookInfoStorage[i + (20 * (countForBookInfoReq-1))].title;
+    var oldImage = bookInfoStorage[i + (20 * (countForBookInfoReq-1))].image;
 
-    bookInfos[i + (20 * (countBookInfoReq-1))]['link'] = urlList[i].url;
+    bookInfoStorage[i + (20 * (countForBookInfoReq-1))]['link'] = shortenedUrlList[i].url;
     oldDescription = oldDescription.replace(/<b>/g, '');
     oldDescription = oldDescription.replace(/<\/b>/g, '');
     if (oldDescription.length) {
       oldDescription = oldDescription.slice(0,50) + "...";
     }
-    bookInfos[i + (20 * (countBookInfoReq-1))].description = oldDescription;
+    bookInfoStorage[i + (20 * (countForBookInfoReq-1))].description = oldDescription;
     oldTitle = oldTitle.replace(/<b>/g, '');
     oldTitle = oldTitle.replace(/<\/b>/g, '');
-    bookInfos[i + (20 * (countBookInfoReq-1))].title = oldTitle;
-    // if (oldImage)
+    bookInfoStorage[i + (20 * (countForBookInfoReq-1))].title = oldTitle;
   }
-
-  var listVersion = document.querySelector('#listversion');
-  var cardVersion = document.querySelector('#cardVersion');
-  listUp(bookInfos);
+  listUp(bookInfoStorage);
 }
 
 function playURLShortner (infoList) {
-  console.log('*****playURLShortner*****');
   shortURLRequest(infoList[countForURLReq].link);
 }
 
@@ -126,20 +141,21 @@ function makeRequest (keyWord) {
   req.onreadystatechange = function () {
     if (req.readyState === 4) {
       if(req.status === 200) {
-        console.log('*****makeRequest*****');
-        countBookInfoReq++;
-        infosToAdd.length = 0;
-        urlList.length = 0;
-        countForURLReq = 0;
-        infosToAdd = infosToAdd.concat(JSON.parse(req.responseText).items);
-        bookInfos = bookInfos.concat(JSON.parse(req.responseText).items);
-        playURLShortner(JSON.parse(req.responseText).items);
+        if (JSON.parse(req.responseText).items.length !== 0) {
+          countForBookInfoReq++;
+          infosToAdd.length = 0;
+          shortenedUrlList.length = 0;
+          countForURLReq = 0;
+          infosToAdd = infosToAdd.concat(JSON.parse(req.responseText).items);
+          bookInfoStorage = bookInfoStorage.concat(JSON.parse(req.responseText).items);
+          playURLShortner(JSON.parse(req.responseText).items);
+        }
       } else {
         console.log("Error loading infos...");
       }
     }
   };
-  req.open('GET', `http://localhost:3000/v1/search/book/?query=${keyWord}&display=20&start=${1+(20 * countBookInfoReq)}`, true);
+  req.open('GET', `http://localhost:3000/v1/search/book/?query=${keyWord}&display=20&start=${1+(20 * countForBookInfoReq)}`, true);
   req.send();
   req.onreadystatechange();
 }
@@ -149,8 +165,7 @@ function shortURLRequest (url) {
   req.onreadystatechange = function (url) {
     if (req.readyState === 4) {
       if(req.status === 200) {
-        console.log('*****shortURLRequest*****');
-        urlList.push(JSON.parse(req.responseText).result);
+        shortenedUrlList.push(JSON.parse(req.responseText).result);
         if (countForURLReq !== infosToAdd.length-1) {
           countForURLReq++;
           playURLShortner(infosToAdd);
@@ -158,7 +173,6 @@ function shortURLRequest (url) {
           infoModifier();
         }
       } else {
-
         console.log("Error loading URL\n");
       }
     }
